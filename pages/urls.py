ff54e6350f08c98c1d95f8b8cc02c6a975336a29
Tim Kao <pakuya.kao@gmail.com>
2018-10-27 18:00:04 -0400

    added one more page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutYouView
 
 urlpatterns = [
+  path('about/', AboutYouView.as_view(), name='about'),
   path('', HomePageView.as_view(), name='home')
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
   template_name = 'home.html'
+
+class AboutYouView(TemplateView):
+  template_name = 'about.html'
